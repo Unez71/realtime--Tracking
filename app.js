285456a261044f4ae2f033282816cf56acc0ec45
unez71 <mohammedunes94@gmail.com>
2024-11-08 23:26:03 +0530

    completed with a major bug

--- app.js
+++ app.js
@@ -1,16 +1,26 @@
 const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
+const path = require('path')
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
 app.set("view engine", "ejs");
-app.set(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
+
+io.on("connection", function(socket){
+    socket.on("send-location", function(data) {
+        io.emit("receive-location", { id: socket.id, ...data })
+    });
+    socket.on("disconnect", function(socket){
+        io.emit("user-disconnected", socket.id)
+    })
+})
 
 app.get("/", (req, res)=>{
-    res.send("hello woerld")
+    res.render("index")
 })
 
 server.listen(3000)
